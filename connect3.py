9bb96b0ae452d48ff6562f5548065d973dd50063
Thomas Caneday <110267727+ThomasCaneday@users.noreply.github.com>
2024-10-15 10:07:21 -0700

    Initial commit
    Added connect3.py via upload

---
+++ connect3.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# Revised Connection Example - Chapter 2 - connect3.py
+import socket
+
+print("Creating socket . . .")
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("done.")
+except:
+    print("failed to create socket.")
+
+print("Looking up port number . . .")
+try:
+    port = socket.getservbyname('http', 'tcp')
+    print("done.")
+except:
+    print("failed to look up port number.")
+
+print("Connecting to remote host on port %d . . ." %(port))
+try:
+    s.connect(("www.google.com", port))
+    print("done.")
+except:
+    print("failed to connect remote host on port.")
+
+print("Conncected from ", s.getsockname())
+print("Connected to ", s.getpeername())
